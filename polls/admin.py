f373ef0d5c023dff891ef9bed88d7b7decbfb7d6
amshirazi64 <amshirazi64@gmail.com>
2023-07-27 13:17:00 +0330

    republish
    deep learning

--- polls/admin.py
+++ polls/admin.py
@@ -1,10 +1,4 @@
 from django.contrib import admin
 from . import models
 
-class ChoiceInline(admin.TabularInline):
-    model = models.Choice
-    extra = 1
-
-@admin.register(models.Question)
-class QuestionAdmin(admin.ModelAdmin):
-    inlines = [ChoiceInline]
+admin.site.register(models.Question)
--- polls/views.py
+++ polls/views.py
@@ -1,38 +1,42 @@
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.http import Http404
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
-from django.views import generic
-from .import models
 
-class IndexView(generic.ListView):
-    template_name = "polls/index.html"
-    context_object_name = "latest_question_list"
+from .models import Question, Choice
 
-    def get_queryset(self):
-        return models.Question.objects.order_by("-pub_date")
+def index(request):
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list,}
+    return render(request, "polls/index.html", context)
 
-class DetailView(generic.DetailView):
-    model = models.Question
-    template_name = "polls/detail.html"
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question":question})
 
-class ResultsView(generic.DetailView):
-    model = models.Question
-    template_name = "polls/results.html"
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/results.html", {"question":question})
 
 def vote(request, question_id):
-    question = get_object_or_404(models.Question, pk=question_id)
+    question = get_object_or_404(Question, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.POST['choice'])
-    except (KeyError, models.Choice.DoesNotExist):
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
         # Redisplay the question voting form.
-        return render(request, 'polls/detail.html', {
-            'question': question,
-            'error_message': "You didn't select a choice.",
-        })
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
     else:
         selected_choice.votes += 1
         selected_choice.save()
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
